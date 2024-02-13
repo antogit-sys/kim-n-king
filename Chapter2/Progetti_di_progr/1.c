c2fa16a7e668084280145a8bb3f881f7121858ac
antogit-sys <antonioperrgiorgi@gmail.com>
2024-02-13 13:37:41 +0100

    1.c

---
+++ Chapter2/Progetti_di_progr/1.c
@@ -0,0 +1,21 @@
+/*	Progettu di programmaziome
+  
+  1. Scrivete un programma che utilizzi la printf per stampare la 
+      seguente immagine sullo schermo
+ 				  
+                  * 	
+                *
+              *	
+            * * * * * 
+ */
+
+#include <stdio.h>
+int main(void)
+{
+	printf("      *  \n");
+	printf("    *    \n");
+	printf("  *      \n");
+	printf("* * * * *\n");
+	
+return 0;	
+}
