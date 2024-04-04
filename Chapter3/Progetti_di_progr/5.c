227e7c0ef6b4b066ee1e8abdf03b93ecca97e9a8
antogit-sys <antonioperrgiorgi@gmail.com>
2024-04-04 19:05:26 +0200

    my god

--- Chapter3/Progetti_di_progr/5.c
+++ Chapter3/Progetti_di_progr/5.c
@@ -8,16 +8,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <time.h>
+
+#define RANDINT(n_min, n_max) (rand() % ((n_max) - (n_min) + 1) + (n_min))
 
 typedef unsigned long ulong;
 
 ulong** create_matrix(ulong, ulong, bool);
 ulong** fill_matrix(ulong, ulong, ulong, ulong);
+bool checkDuplicate(ulong,ulong**,ulong,ulong);
+void printMatrix(ulong**, ulong, ulong);
+void freeMatrix(ulong**, ulong);
+//void sumMatrix(bool,ulong**, ulong, ulong);
 
 int main(void)
 {
-    int** m = (int**)create_matrix(4,4, true);
-    
+    int r=4,c=4;
+
+    srand(time(NULL));
+    ulong** m = (ulong**)create_matrix(r,c, true);
+    printMatrix(m, r,c);
+    //sum_matrix("Rows sums: ",m,r);
+
+    freeMatrix(m,r);
 
 return EXIT_SUCCESS;
 }
@@ -44,10 +57,54 @@ ulong** create_matrix(ulong r, ulong c, bool f_fill){
     return m;
 }
 
-
 ulong** fill_matrix(ulong n_min, ulong n_max, ulong r, ulong c){
     if(r*c != n_max-n_min+1){
         fprintf(stderr,"Error number maximum is less of max number element\n");
         exit(1);
     }
+    
+    ulong** mlocal = (ulong**)create_matrix(r,c,false);
+
+    for(size_t i=0; i<r; ++i){
+        for(size_t j=0; j<c; ++j){
+            ulong num;
+            do{
+                num = RANDINT(n_min, n_max);
+            }while(checkDuplicate(num, mlocal, r,c));
+            mlocal[i][j] = num;
+        }
+    }
+return mlocal;
 }
+
+
+bool checkDuplicate(ulong num, ulong** m, ulong rows, ulong columns){
+    bool done = false;
+    for(size_t i=0; i<rows; ++i){
+        for(size_t j=0; j<columns; ++j){
+            (m[i][j] == num)?done=true:0;
+        }
+    }
+return done;
+}
+
+void printMatrix(ulong **m, ulong r, ulong c){
+    for(size_t i=0; i<r; ++i){
+        for(size_t j=0; j<c; ++j)
+            printf("%2lu ",m[i][j]);
+        putchar('\n');
+    }
+}
+
+void freeMatrix(ulong** m, ulong r) {
+    for (size_t i = 0; i < r; ++i) {
+        free(m[i]);
+    }
+    free(m);
+}
+
+/*void sumMatrix(bool f_rows, ulong** m, ulong r, ulong c){
+    ulong rorc; //rows or columns
+    
+    (f_rows == true)?rorc = r: rorc = c;
+}*/
