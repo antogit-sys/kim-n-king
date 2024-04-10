3826bce278f5c7a7e7469cca80eb5540f52ddbb4
antogit-sys <antonioperrgiorgi@gmail.com>
2024-04-10 20:35:56 +0200

    add trace XD

--- Chapter5/Progetti_di_progr/2.c
+++ Chapter5/Progetti_di_progr/2.c
@@ -1,3 +1,16 @@
+/*
+ * - Progetti di prog
+ *
+ *   2. Write a program that asks the user for a 24-hour time, then displays the time in 12-hour form:
+ *
+ *          Enter a 24-hour time: 21:11
+ *          Equivalent 12-hour time: 9:11 PM
+ *      
+ *      Be careful not to display 12:00 as 0:00.
+ *
+ *   ;)
+ */
+
 #include <stdio.h>
 
 // Macro for converting time to 12-hour format
