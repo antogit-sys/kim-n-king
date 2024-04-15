9b25a1e50d33847239744a4e8512b32d9e791ba0
antogit-sys <antonioperrgiorgi@gmail.com>
2024-04-15 15:53:31 +0200

    fix function

--- Chapter5/Progetti_di_progr/7.c
+++ Chapter5/Progetti_di_progr/7.c
@@ -4,21 +4,28 @@
 void analyze_of_array(int*, int, int*, int*);
 int main(void)
 {
-    int v[N]={21,48,10,35,1};
+    //int v[N]={1,2,3,4,5};
+    //int v[N]={21,48,10,35,1};
+    //int v[N]={2,2,2,2,2};
+    int v[N]={2, 3, 9, 7, 1};
+
     int min,max;
 
     analyze_of_array(v, N, &min, &max);
     
-    printf("%d\n%d",min,max);
+    printf("Largest: %d\n",max);
+    printf("Smallest: %d\n",min);
     
 
 return 0;
 }
 
 void analyze_of_array(int* v, int size, int* min, int* max){
-    *min = v[0];
-    *max = v[0];
-
-    for(size_t i=1; i<size; ++i)
-        *max = (*min<v[i])?*min=v[i]:v[i];
+    
+    *min = *max = v[0];
+    
+    for(size_t i=1; i<size; ++i){
+        *min = (*min>v[i])?v[i]:*min;
+        *max = (*max<v[i])?v[i]:*max;
+    }
 }
