c31f8d6506bce911cac084cbda00db893b34290e
antogit-sys <antonioperrgiorgi@gmail.com>
2024-04-15 17:17:50 +0200

    add trace

--- Chapter5/Progetti_di_progr/7.c
+++ Chapter5/Progetti_di_progr/7.c
@@ -1,3 +1,13 @@
+/* Progetti di prog
+ *
+ * 7. Write a program that finds the largest and smallest of four integers entered by the user:
+ *
+ *       Enter four integers: 21 42 10 35
+ *       Largest: 43
+ *       Smallest: 10
+ *
+ *   Use as few if statements as possible: Hint: Four if statements are sufficient.
+ */
 #include <stdio.h>
 #define N 5
 
