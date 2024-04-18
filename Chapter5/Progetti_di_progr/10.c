ac14b4e2f62f38d8cac12fab63ffd64536813a1b
antogit-sys <antonioperrgiorgi@gmail.com>
2024-04-18 20:02:32 +0200

    create CALC_GRADE macro

--- Chapter5/Progetti_di_progr/10.c
+++ Chapter5/Progetti_di_progr/10.c
@@ -2,12 +2,20 @@
 #include <stdlib.h>
 
 #define BETWEEN(a,n1,n2) (a>=n1 && a<=n2)
- 
+
+#define CALC_GRADE(n)\
+    (BETWEEN(n,90,100)?"A":\
+     (BETWEEN(n,80,89)?"B": \
+      (BETWEEN(n,70,79)?"C": \
+       (BETWEEN(n,60,69)?"D": \
+        (BETWEEN(n,0,59)?"F":"**ERROR CALC GRADE***")))))
+
 int main(void)
 {
     int n=10;
     
-
+    printf("%s",CALC_GRADE(n));
+    
 
 return EXIT_SUCCESS;
 }
