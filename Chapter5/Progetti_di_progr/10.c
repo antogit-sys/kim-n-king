1a0b3ffc3f0e7de7f62a516f9d4cb183d623a643
antogit-sys <antonioperrgiorgi@gmail.com>
2024-04-18 21:18:01 +0200

    const char* -> const char and NACK implement

--- Chapter5/Progetti_di_progr/10.c
+++ Chapter5/Progetti_di_progr/10.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Negative Acknowledgement */
+#define NACK 21 
+
 #define BETWEEN(a,n1,n2) (a>=n1 && a<=n2)
 
 #define __CALC_GRADE(n)\
-    (BETWEEN(n,90,100)?"A":\
-     (BETWEEN(n,80,89)?"B": \
-      (BETWEEN(n,70,79)?"C": \
-       (BETWEEN(n,60,69)?"D": \
-        (BETWEEN(n,0,59)?"F":NULL)))))
+    (BETWEEN(n,90,100)?'A':\
+     (BETWEEN(n,80,89)?'B': \
+      (BETWEEN(n,70,79)?'C': \
+       (BETWEEN(n,60,69)?'D': \
+        (BETWEEN(n,0,59)?'F':NACK)))))
 
 #define EXIT_IF(c, s)\
     do{\
@@ -18,7 +21,7 @@
         }\
     }while(0)  
 
-const char* calc_grade(int);
+const char calc_grade(int);
 
 int main(void)
 {
@@ -27,15 +30,15 @@ int main(void)
     fputs("Enter numerical grade: ",stdout);
     fscanf(stdin,"%d",&n);
 
-    fprintf(stdout,"Letter grade: %s\n",calc_grade(n));
+    fprintf(stdout,"Letter grade: %c\n",calc_grade(n));
 
 return EXIT_SUCCESS;
 }
 
-const char* calc_grade(int n){
-    const char* letter = __CALC_GRADE(n);
-
-    EXIT_IF(letter == NULL, "Error calc_grade: Invalid grade range!");
+const char calc_grade(int n){
+    const char letter = __CALC_GRADE(n);
+    
+    EXIT_IF(letter == NACK, "Error calc_grade: Invalid grade range!\n");
     
     return letter;
 }
