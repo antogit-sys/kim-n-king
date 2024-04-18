afc57aefabd4fa9b1e8c3edcce74f95dce988e0f
antogit-sys <antonioperrgiorgi@gmail.com>
2024-04-18 19:29:46 +0200

    add trace 9

--- Chapter5/Progetti_di_progr/9.c
+++ Chapter5/Progetti_di_progr/9.c
@@ -1,3 +1,13 @@
+/* Prog di prog
+ *
+ * 9. Write a program that prompts the user to enter two dates and then indicates which date comes earlier on the calandar:
+ *  
+ *  Enter first date (mm/dd/yyyy): 3/6/08
+ *  Enter second date (mm/dd/yyyy): 5/17/07
+ *
+ *  5/17/07 is earlier than 3/6/08
+ *
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
