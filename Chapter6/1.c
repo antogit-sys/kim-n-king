78066e342bda343378e0ff2b52b5da0736e8a3c9
antogit-sys <antonioperrgiorgi@gmail.com>
2024-04-23 18:03:27 +0200

    add trace

--- Chapter6/1.c
+++ Chapter6/1.c
@@ -1,3 +1,21 @@
+/* - Progetti di prog
+ *
+ *   1. prompt the user to enter numbers one by one, 
+ *   When the user enters 0 or a negative number, the 
+ *   program must display the largest nonnegative number 
+ *   entered:
+ *      
+ *      Enter a number: 60
+ *      Enter a number: 38.3
+ *      Enter a number: 4.89
+ *      Enter a number: 100.62
+ *      Enter a number: 75.2295
+ *      Enter a number: 0
+ *
+ *   The largest number entered was 100.62
+ *   Notice that the numbers aren't necessarily integers.
+*/
+
 #include <stdio.h>
 
 #define NUM_MAX_POSITIVE(n, max) if(n > 0) max = max>n?max:n
