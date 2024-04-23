17194ec5329b9eed9bfafb52e851f924a2c2da65
antogit-sys <antonioperrgiorgi@gmail.com>
2024-04-23 19:18:19 +0200

    add trace 4.c

--- Chapter6/4.c
+++ Chapter6/4.c
@@ -1,3 +1,20 @@
+/* - Prog di prog
+ *
+ *  4. Add a loop to the broker.c program of 
+ *  Section 5.2 so that the user can enter more 
+ *  than one trade and the program will caluclate 
+ *  the commission on each. The program should 
+ *  terminate when the user enters 0 as the trade value:
+ *
+ *      Enter value of trade: 30000
+ *      Commission: $166.00
+ *
+ *      Enter value of trade: 20000
+ *      Commission: $144.00
+ *
+ *      Enter value of trade: 0
+ */
+
 /* Calculates a broker's commission */
 
 #include <stdio.h>
