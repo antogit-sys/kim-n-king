981d1834e444c1d92e0f6d3128befda99d9dada1
antogit-sys <antonioperrgiorgi@gmail.com>
2024-04-30 18:36:57 +0200

    add 11.c

---
+++ Chapter6/11.c
@@ -0,0 +1,33 @@
+/* Prog di prog
+ *
+ * 11. The value of the mathematical constant e 
+ * can be expressed as an infinite series:
+ *
+ *  e = 1 + 1/1! + 1/2! + 1/3! + ...
+ *
+ * Write a program that approximates e by 
+ * computing the value of
+ *  
+ *  1 + 1/1! + 1/2! + 1/3! + ... + 1/n!
+ *
+ * where n is an integer entered by the user.
+ *
+ */
+
+#include <stdio.h>
+#define FACT(n) (n*n)
+int main(void)
+{
+    float e;
+    int n;
+
+    fputs("n? ",stdout);
+    scanf("%d",&n);
+
+    for(int i = 1, den=1; i <= n; ++i){
+        e += 1.0f / (den*=FACT(i));
+    }
+
+    printf("e: %f\n", e);
+
+}
