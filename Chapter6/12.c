f13cdde0e651547a9f9b8a3c3d0308b7a1a6084c
antogit-sys <antonioperrgiorgi@gmail.com>
2024-05-02 18:31:19 +0200

    fuck u

---
+++ Chapter6/12.c
@@ -0,0 +1,32 @@
+/* Prog di prog
+ *
+ * 12. Modify Programming Project 11 so that the program 
+ * continues adding terms until the current term becomes 
+ * less than epsilon, where epsilon is a small (floating-point) 
+ * number entered by the user.
+ *
+ */
+
+#include <stdio.h>
+
+int quad(int n){return n*n;}
+
+int main(void)
+{
+    puts("fuck u!");
+    /*
+    float e=0.0f,E;
+    float current=1.0f;
+    int i = 1;
+    
+    fputs("E? ",stdout);
+    scanf("%f",&E);
+
+    do{
+        e += current;
+        current = 1.0f / quad(i++);
+        printf("e%d: %f\n",i,e);
+    }while(current>=E);
+    */
+return 0;
+}
