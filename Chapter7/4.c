90c2cf1c699bf1d8128aedda1ff84e4c02d87324
antogit-sys <antonioperrgiorgi@gmail.com>
2024-05-07 18:37:24 +0200

    add 4.c

---
+++ Chapter7/4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <ctype.h>
+void print_num(const char*);
+
+int main(void)
+{
+    print_num("Enter a phone number: ");
+    putchar('\n');
+
+return 0;
+}
+
+void print_num(const char *s){
+    int ch;
+
+    fputs(s, stdout);
+    fflush(stdout);
+    
+    while((ch=getchar()) != '\n'){
+        ch = (ch>='a' && ch<='z')? toupper(ch): ch;
+        switch(ch){
+            case 'A': case 'B': case 'C': putchar('2');
+                                          break;
+            case 'D': case 'E': case 'F': putchar('3');
+                                          break;
+            case 'G': case 'H': case 'I': putchar('4');
+                                          break;
+            case 'J': case 'K': case 'L': putchar('5');
+                                          break;
+            case 'M': case 'N': case 'O': putchar('6');
+                                          break;
+            case 'P': case 'R': case 'S': putchar('7');
+                                          break;
+            case 'T': case 'U': case 'V': putchar('8');
+                                          break;
+            case 'W': case 'X': case 'Y': putchar('9');
+                                          break;
+            default:
+                putchar(ch);
+                break;
+            }
+    }
+
+}
