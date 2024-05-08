ba78821656795ff7dec131c71bb79c57a1122792
antogit-sys <antonioperrgiorgi@gmail.com>
2024-05-08 20:02:43 +0200

    add 9.c trace

--- Chapter7/9.c
+++ Chapter7/9.c
@@ -1,3 +1,16 @@
+/* Prog di prog
+ *
+ * 9. Write a program that asks the user for a 12-hour 
+ * time, then displays the time in 24-hour form:
+ *
+ *  Enter a 12-hour time: 9:11 PM
+ *  Equivalent 24-hour time: 21:11
+ *
+ *  See Programming Project 8 for a description of the 
+ *  input format.
+ *
+ **/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
