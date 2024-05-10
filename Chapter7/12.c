d14c935a1a39947ee0eea8d1cdda80ee282b4fbc
antogit-sys <antonioperrgiorgi@gmail.com>
2024-05-10 22:23:26 +0200

    add 12.c trace

--- Chapter7/12.c
+++ Chapter7/12.c
@@ -1,3 +1,16 @@
+/* Prog di prog
+ *
+ * 12. Write a program that evaluates an expression:
+ *  
+ *  Enter an expression: 1+2.5*3
+ *  Value of expression: 10.5
+ *
+ * The operands in the expression are floating-point 
+ * numbers; the operators are +, -, * and /. The expression 
+ * is evalued from left to right (no operator taes precedence 
+ * over any other operator).
+ **/
+
 #include <stdio.h>
 
 int main(void)
