96da6604962abd5584c8224f1a8e725af8dc92e3
antogit-sys <antonioperrgiorgi@gmail.com>
2024-05-11 17:30:20 +0200

    add 13.c trace

--- Chapter7/13.c
+++ Chapter7/13.c
@@ -1,3 +1,16 @@
+/* Prog di prog
+ *
+ * 14. Write a program that calculates the 
+ * average word length for a sentence:
+ *
+ *  Enter a sentence: It was deja vu all over again.
+ *  Average word length: 3.4
+ *
+ * For simplicity, your program should consider 
+ * a punctuation mark to be part of the word to 
+ * which it is attached. Display the average word 
+ * length to one decimal place.
+ */
 
 #include <stdio.h>
 
