9fa44b184e6e05142a5dbc6a1d1cf374a1b5951b
antogit-sys <antonioperrgiorgi@gmail.com>
2024-05-14 20:15:09 +0200

    add 3.c trace

--- Chapter8/3.c
+++ Chapter8/3.c
@@ -1,3 +1,12 @@
+/* Prog di prog 8
+ *
+ * 3. Modify the repdigit.c program of Section 8.1 so 
+ * that the user can enter more than one number to be 
+ * tested for repeated digits. The program should terminate 
+ * when the user enters a number that's less than or equal to 0.
+ *
+ **/
+
 /* Checks numbers for repeated digits */
 
 #include <stdbool.h>   /* C99 only */
