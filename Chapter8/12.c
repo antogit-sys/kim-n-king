ae246c145ae81b3b034a5d774344a70d445f863f
antogit-sys <antonioperrgiorgi@gmail.com>
2024-05-22 16:07:52 +0200

    add 12.c trace

--- Chapter8/12.c
+++ Chapter8/12.c
@@ -1,16 +1,14 @@
 /* -Prog di prog
  *
- * 5. In the SCRABBLE Crossword Game, players form words using small tiles, 
- * each containing a letter and a face value. The face value varies from one 
- * letter to another, based on the letter's rarity. (Here are the face values: 
- * 1: AEILNORSTU, 2: DG, 3: BCMP, 4: FHVWY, 5: K, 8: JX, 10: QZ.) Write a program 
- * that computes the value of a word by summing the values of its letters:
- *
- *  Enter a word: pitfall
- *  Scrabble value: 12
- *
- * Your program should allow any mixture of lower-case and upper-case letters in 
- * the word. Hint: Use the toupper library function.
+ * 12. Modify Programming Project 5 from Chapter 7 
+ * so that the SCRABBLE values of the letters are 
+ * stored in an array. The array wil have 26 elements, 
+ * corresponding to the 26 letters of the alphabet. For 
+ * example, element 0 of the array will store 1 (because 
+ * the SCRABBLE value of the letter B is 3), and so forth. 
+ * As each character of the input word is read, the program 
+ * will use the array to determine the SCRABBLE value of that 
+ * character. Use an array initializer to set up the array.
  *
  **/
 #include <stdio.h>
