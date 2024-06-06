c29c577bac2e0aecc367ac092c73efc7dee8ca91
antogit-sys <antonioperrgiorgi@gmail.com>
2024-06-06 18:26:21 +0200

    add function out_reversal_sentence

--- Chapter8/14.c
+++ Chapter8/14.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define OCA(v,n)  for(size_t i=0; i<n; ++i) putchar(v[i])
 #define EXIT_IF(c, s)\
     do{\
         if((c)){\
@@ -11,22 +10,27 @@
     }while(0)
 
 #define IN(a,op,v1,v2,v3) (a op v1 && a op v2 && a op v3)
+#define NEW_ARRAY(t,n)  ((t*)calloc(n,sizeof(t)))
 
 #define N 100
 
 size_t input_stdi(char*, char*);
-void reversal_sentence(char*, size_t n, char*);
-int main(void) {
+void out_reversal_sentence(char*, int, char);
 
-    char c, c_term, words[N] = {0};
+int main(void)
+{
+    char* words = NEW_ARRAY(char, N);
+    char c_term;
+    int size = 0;
 
     fputs("Enter a sentence: ",stdout);
-    i = input_stdi(words,&c_term);
+    size = input_stdi(words,&c_term);
 
     EXIT_IF(IN(c_term, != ,'.','?','!'), "Error invalid final charter!");
 
     fputs("Reversal of sentence: ",stdout);
-
+    out_reversal_sentence(words, size, c_term);
+    putchar('\n');
 
     return 0;
 }
@@ -37,11 +41,23 @@ size_t input_stdi(char* v, char* term){
         v[i++]=ch;
         *term = ch;
     }
+    v[i] = '\0';
     return i;
 }
 
-void reversal_sentence(char* v, size_t n, char* term){
+void out_reversal_sentence(char *string, int str_len, char c_term){
+    int i = str_len - 1;
 
+    EXIT_IF(N<i, "***Error size max size 100!***");
 
-
+    while(i-->= 0){
+        if(string[i - 1] == ' ' || i == 0){
+            int j = i;
+            while(string[j] != ' ' && j < str_len-1)
+                putchar(string[j++]);
+            if(i > 0)
+                putchar(' ');
+        }
+    }
+    putchar(c_term);
 }
